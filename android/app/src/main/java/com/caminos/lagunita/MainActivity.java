a204d9159d6e5dccc1daffc8d11578601393bf16
AppSoftwares Developer <developer@appsoftwares.com>
2026-06-26 13:03:37 -0400

    Include native projects in repository for CI

---
+++ android/app/src/main/java/com/caminos/lagunita/MainActivity.java
@@ -0,0 +1,11 @@
+package com.caminos.lagunita;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
